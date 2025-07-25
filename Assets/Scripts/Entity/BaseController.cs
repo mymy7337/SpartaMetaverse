2a4c93561539da2b710bacd9b292f2f7fe7ac022
mymy7337 <minyeong7337@gmail.com>
2025-07-25 12:20:33 +0900

    [Feat] BaseController, StatHandler 연동

--- Assets/Scripts/Entity/BaseController.cs
+++ Assets/Scripts/Entity/BaseController.cs
@@ -21,10 +21,13 @@ public class BaseController : MonoBehaviour
 
     protected AnimationHandler animationHandler;
 
+    protected StatHandler statHandler;
+
     protected virtual void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         animationHandler = GetComponent<AnimationHandler>();
+        statHandler = GetComponent<StatHandler>();
     }
 
     protected virtual void Start()
@@ -54,7 +57,7 @@ public class BaseController : MonoBehaviour
 
     private void Movement(Vector2 direction)
     {
-        direction = direction * 5;
+        direction = direction * statHandler.Speed;
         if (knockbackDuration > 0.0f)
         {
             direction *= 0.2f;
